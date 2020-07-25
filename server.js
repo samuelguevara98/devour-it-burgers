2cecfc384f817c71b52a79b72557a6cbbe91066f
Samuel Guevara <samsjamin@outlook.com>
2020-07-25 18:06:06 -0400

    created routes and port

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+var PORT = process.envPORT || 8080;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("SERVER LISTENING ON: http:localhost:" + PORT);
+})
